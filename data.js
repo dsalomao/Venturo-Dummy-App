b30230075145ead119fcc515ea7d936ed025630d
daniel <dsalomao@users.noreply.github.com>
2017-09-10 15:08:58 -0300

    main files

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+    return {
+        bikers: [
+            { id: 1, fullname: "Isac Newton", email: "neutron@example.com", city: "Campinas",
+                rideInGroup: "Always", sun: true, mon: true, tue: true, wed: true, thu: true, fri: true, sat: true, registration: 1505051436310},
+            { id: 2, fullname: "carl Wheezer", email: "carl@example.com", city: "Campinas",
+                rideInGroup: "Sometimes", sun: false, mon: true, tue: true, wed: true, thu: true, fri: true, sat: false, registration: 1505051436310},
+            { id: 3, fullname: "Cindy Vortex", email: "cindyvortex@example.com", city: "São Paulo",
+                rideInGroup: "Never", sun: true, mon: false, tue: false, wed: false, thu: false, fri: false, sat: true, registration: 1505051436310},
+            { id: 4, fullname: "Sheen Estevez", email: "sheen@example.com", city: "São Paulo",
+                rideInGroup: "Sometimes", sun: false, mon: true, tue: false, wed: true, thu: false, fri: true, sat: false, registration: 1505051436310},
+            { id: 5, fullname: "Libby Folfax", email: "folfax@example.com", city: "Americana",
+                rideInGroup: "Sometimes", sun: false, mon: true, tue: true, wed: true, thu: false, fri: false, sat: false, registration: 1505051436310},
+            { id: 6, fullname: "Nick Dean", email: "nickd@example.com", city: "Sumaré",
+                rideInGroup: "Always", sun: true, mon: false, tue: false, wed: false, thu: false, fri: false, sat: true, registration: 1505051436310}
+        ]
+    }
+}
